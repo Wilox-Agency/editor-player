b02f3b11e97b01d1f08b40fb3112883a21423b84
heitorlisboa <heitor01101000@gmail.com>
2024-01-29 08:50:36 -0300

    chore: rename variable for better understanding

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -11,9 +11,7 @@ export function validateUrl(string: string) {
 
 export function validateAssetUrl(type: 'image' | 'video', url: string) {
   return new Promise<boolean>((resolve) => {
-    const imageElement = document.createElement(
-      type === 'image' ? 'img' : 'video'
-    );
+    const element = document.createElement(type === 'image' ? 'img' : 'video');
     const loadEventName = type === 'image' ? 'load' : 'loadedmetadata';
 
     function handleLoad() {
@@ -27,14 +25,14 @@ export function validateAssetUrl(type: 'image' | 'video', url: string) {
     }
 
     function cleanup() {
-      imageElement.removeEventListener(loadEventName, handleLoad);
-      imageElement.removeEventListener('error', handleError);
-      imageElement.remove();
+      element.removeEventListener(loadEventName, handleLoad);
+      element.removeEventListener('error', handleError);
+      element.remove();
     }
 
-    imageElement.addEventListener(loadEventName, handleLoad);
-    imageElement.addEventListener('error', handleError);
-    imageElement.src = url;
+    element.addEventListener(loadEventName, handleLoad);
+    element.addEventListener('error', handleError);
+    element.src = url;
   });
 }
 

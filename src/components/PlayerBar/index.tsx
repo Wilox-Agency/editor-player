ddf9d3c4129264bc519f405e79fc6021189367f6
heitorlisboa <heitor01101000@gmail.com>
2024-03-27 17:05:07 -0300

    feat(player): automatically hide player bar after some time of inactivity when timeline is playing

--- src/components/PlayerBar/index.tsx
+++ src/components/PlayerBar/index.tsx
@@ -1,6 +1,20 @@
-import { useEffect } from 'react';
+import {
+  type RefObject,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
+import { create } from 'zustand';
 import * as Toolbar from '@radix-ui/react-toolbar';
 import * as Popover from '@radix-ui/react-popover';
+import {
+  useFocusVisible,
+  useFocusWithin,
+  useHover,
+} from '@react-aria/interactions';
+import { mergeProps } from '@react-aria/utils';
 import { Pause, Play, Volume1, Volume2, VolumeX } from 'lucide-react';
 
 import styles from './PlayerBar.module.css';
@@ -20,11 +34,17 @@ export function PlayerBar({
 }) {
   const { timelineCurrentTime, timelineDuration, timelineState } =
     usePlayerTimelineStore();
+
   const volume = usePlayerAudioStore((state) => state.volume);
   const setVolume = usePlayerAudioStore((state) => state.setVolume);
-
   // Use a different volume icon depending on the volume
   const VolumeIcon = volume === 0 ? VolumeX : volume <= 0.5 ? Volume1 : Volume2;
+
+  const playerBarRef = useRef<HTMLDivElement>(null);
+  const { visibility, playerBarProps } = usePlayerBarVisibility({
+    playerBarRef,
+    isTimelinePlaying: timelineState === 'playing',
+  });
 
   useEffect(() => {
     const { currentAudio } = usePlayerAudioStore.getState();
@@ -34,7 +54,15 @@ export function PlayerBar({
   }, [volume]);
 
   return (
-    <Toolbar.Root className={styles.playerBar}>
+    <Toolbar.Root
+      className={styles.playerBar}
+      style={{
+        display: visibility === 'hidden' ? 'none' : undefined,
+        opacity: visibility === 'visible' ? 1 : 0,
+      }}
+      {...playerBarProps}
+      ref={playerBarRef}
+    >
       <Toolbar.Button
         className={styles.playerBarButton}
         onClick={handlePlayOrPause}
@@ -100,4 +128,182 @@ function formatTime(timeInSeconds: number) {
     .toString()
     .padStart(2, '0');
   return `${minutes}:${seconds}`;
+}
+
+function useIsMouseMoving() {
+  const [isMouseMoving, setIsMouseMoving] = useState(false);
+
+  const mouseIdleTimeoutRef = useRef<number>();
+
+  useEffect(() => {
+    function handleMouseMove(event: PointerEvent) {
+      if (event.pointerType !== 'mouse') return;
+
+      clearTimeout(mouseIdleTimeoutRef.current);
+      setIsMouseMoving(true);
+      mouseIdleTimeoutRef.current = setTimeout(() => {
+        setIsMouseMoving(false);
+      }, 100);
+    }
+
+    window.addEventListener('pointermove', handleMouseMove);
+    return () => window.removeEventListener('pointermove', handleMouseMove);
+  }, []);
+
+  return { isMouseMoving };
+}
+
+function useFocusVisibleWithin() {
+  const { isFocusVisible } = useFocusVisible();
+  const [isFocusWithin, setIsFocusWithin] = useState(false);
+  const { focusWithinProps } = useFocusWithin({
+    onFocusWithinChange: setIsFocusWithin,
+  });
+
+  const isFocusVisibleWithin = useMemo(
+    () => isFocusVisible && isFocusWithin,
+    [isFocusVisible, isFocusWithin]
+  );
+
+  return { isFocusVisibleWithin, focusVisibleWithinProps: focusWithinProps };
+}
+
+const hideDelayInSeconds = 3;
+const hideAnimationDurationInSeconds = 0.15;
+
+type Visibility = 'visible' | 'hiding' | 'hidden';
+type PlayerVisibilityStore = {
+  visibility: Visibility;
+  setVisibility: (visibility: Visibility) => void;
+};
+
+/* Using a store to be able to get the state using a snapshot instead of using a
+"reactive" variable */
+const usePlayerVisibilityStore = create<PlayerVisibilityStore>((set) => ({
+  visibility: 'visible',
+  setVisibility: (visibility) => {
+    set({ visibility });
+  },
+}));
+
+// TODO: Instantly hide the player bar if the mouse moves out of the window
+function usePlayerBarVisibility({
+  playerBarRef,
+  isTimelinePlaying,
+}: {
+  playerBarRef: RefObject<HTMLElement>;
+  isTimelinePlaying: boolean;
+}) {
+  const { visibility, setVisibility } = usePlayerVisibilityStore();
+  const hidingTimeoutRef = useRef<number>();
+  const hideTimeoutRef = useRef<number>();
+
+  const { isHovered, hoverProps } = useHover({});
+  const { isFocusVisibleWithin, focusVisibleWithinProps } =
+    useFocusVisibleWithin();
+  const { isMouseMoving } = useIsMouseMoving();
+
+  const hidePlayerBar = useCallback(
+    ({ withTimeout = false }: { withTimeout?: boolean } = {}) => {
+      /* Using state inside a function redefines it on every render, and
+      redefining the function re-runs any effect that has it as a dependency,
+      which in this case is undesired, so a snapshot of the state is being used
+      instead of a "reactive" variable */
+      const { visibility, setVisibility } = usePlayerVisibilityStore.getState();
+      // If the player bar is already hidden (or is hiding), do nothing
+      if (visibility !== 'visible') return;
+
+      function hide() {
+        // Start hiding
+        setVisibility('hiding');
+
+        // Set as hidden when the hide animation finished
+        hideTimeoutRef.current = setTimeout(() => {
+          setVisibility('hidden');
+        }, hideAnimationDurationInSeconds * 1000);
+      }
+
+      if (!withTimeout) {
+        hide();
+        return;
+      }
+
+      hidingTimeoutRef.current = setTimeout(hide, hideDelayInSeconds * 1000);
+    },
+    []
+  );
+
+  // Toggle player bar visibility when touching outside of it,
+  useEffect(() => {
+    // TODO: Do nothing if volume slider is open
+    function handleTouchOutside(event: TouchEvent) {
+      const isPlayerBarTouch = playerBarRef.current?.contains(
+        event.target as HTMLElement | null
+      );
+      if (isPlayerBarTouch) return;
+
+      clearTimeout(hidingTimeoutRef.current);
+      clearTimeout(hideTimeoutRef.current);
+
+      if (visibility !== 'visible') {
+        setVisibility('visible');
+
+        // If the timeline is still playing, hide the player bar after a timeout
+        if (isTimelinePlaying) {
+          hidePlayerBar({ withTimeout: true });
+        }
+        return;
+      }
+
+      hidePlayerBar();
+    }
+
+    window.addEventListener('touchstart', handleTouchOutside);
+    return () => window.removeEventListener('touchstart', handleTouchOutside);
+  }, [
+    hidePlayerBar,
+    isTimelinePlaying,
+    playerBarRef,
+    setVisibility,
+    visibility,
+  ]);
+
+  // Toggle player bar visibility depending on various states
+  useEffect(() => {
+    // TODO: Also require the volume slider to be closed to set the hide timeout
+    const shouldSetHideTimeout =
+      isTimelinePlaying &&
+      !isHovered &&
+      !isFocusVisibleWithin &&
+      !isMouseMoving;
+    if (!shouldSetHideTimeout) {
+      clearTimeout(hidingTimeoutRef.current);
+      clearTimeout(hideTimeoutRef.current);
+      setVisibility('visible');
+      return;
+    }
+
+    hidePlayerBar({ withTimeout: true });
+  }, [
+    hidePlayerBar,
+    isFocusVisibleWithin,
+    isHovered,
+    isMouseMoving,
+    isTimelinePlaying,
+    setVisibility,
+  ]);
+
+  // Hide cursor when player bar is hidden (or is hiding)
+  useEffect(() => {
+    if (visibility !== 'visible') {
+      document.body.style.cursor = 'none';
+    } else {
+      document.body.style.removeProperty('cursor');
+    }
+  }, [visibility]);
+
+  return {
+    visibility,
+    playerBarProps: mergeProps(hoverProps, focusVisibleWithinProps),
+  };
 }
